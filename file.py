2088170c39e01cdfc87885ee01ab6d4411247cb7
rekebuba <abukiabdullahi1@gmail.com>
2024-01-15 22:57:34 +0300

    exercise

--- file.py
+++ file.py
@@ -3,15 +3,20 @@ import os
 def valid(n):
     n = int(n)
     result = 0
-    if n % 2 == 0:
+    
+    if n % 10 == 0:
+        result = n / 10
+        return (round(result), 10)
+    elif n % 2 == 0:
         result = n / 2
-    return result
+        return (round(result), 2)
+    
 
 def main():
     file = open("123.txt", "r")
     for line in file:
         number = line.strip('\n')
         n = valid(number)
-        print(f"{number} = {n} * {2}")
+        print(f"{number} = {n[0]} * {n[1]}")
     file.close()
 main()
