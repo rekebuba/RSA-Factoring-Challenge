78d712bafd5b015eefe19c65ff30bc6a8ad7c4ac
rekebuba <abukiabdullahi1@gmail.com>
2024-01-25 22:17:17 +0300

    rsa

--- rsa.c
+++ rsa.c
@@ -1,11 +1,15 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 
-bool is_prime(unsigned long int num);
-bool are_both_primes(unsigned long int num1, unsigned long int num2);
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+bool miller_rabin(unsigned long long int num, int k);
+unsigned long long int power(unsigned long long int x, unsigned long long int y, unsigned long long int p);
+bool is_prime(unsigned long long int num);
+bool are_both_primes(unsigned long long int num1, unsigned long long int num2);
+
 /**
  * main - Factorize as many numbers as possible
  * into a product of two smaller numbers.
@@ -22,24 +26,23 @@ int main(int argc, char **argv)
     }
     ssize_t read;
     size_t n = 0;
-    unsigned long int num, i;
+    unsigned long long int num, i;
     char *buffer = NULL, *endptr;
-
     while ((read = getline(&buffer, &n, file)) != -1)
     {
         buffer[strlen(buffer) - 1] = '\0';
         if (strlen(buffer) < 20)
         {
-            num = strtoul(buffer, &endptr, 10);
+            num = strtoll(buffer, &endptr, 10);
 
             for (i = 2; i <= num / 2; i++)
             {
-                if (num % i == 0 && are_both_primes(i, (num / i)))
+                if (num % i == 0 && are_both_primes((num / i), i))
                 {
-                    printf("%lu=%lu*%lu\n", num, (num / i), i);
+                    printf("%llu=%llu*%llu\n", num, (num / i), i);
                     break;
                 }
-            }          
+            }
         }
     }
     fclose(file);
@@ -47,10 +50,15 @@ int main(int argc, char **argv)
     return (0);
 }
 
-
-bool is_prime(unsigned long int num)
+/**
+ * is_prime - check if a number is prime
+ *
+ * @num: the number
+ * Return: true or false
+ */
+bool is_prime(unsigned long long int num)
 {
-    for (unsigned long int i = 2; i * i <= num; i++)
+    for (unsigned long long int i = 2; i * i <= num; i++)
     {
         if (num % i == 0)
             return false;
@@ -59,7 +67,84 @@ bool is_prime(unsigned long int num)
     return true;
 }
 
-bool are_both_primes(unsigned long int num1, unsigned long int num2)
+/**
+ * is_prime - check if a number is prime
+ *
+ * @num1: the first number
+ * @num2: the second number
+ * Return: true or false
+ */
+bool are_both_primes(unsigned long long int num1, unsigned long long int num2)
 {
-    return is_prime(num1) && is_prime(num2);
+    return miller_rabin(num1, 5) && miller_rabin(num2, 5);
+}
+unsigned long long int power(unsigned long long int x, unsigned long long int y, unsigned long long int p)
+{
+    unsigned long long int res = 1;
+    x = x % p;
+
+    while (y > 0)
+    {
+        if (y & 1)
+        {
+            res = (res * x) % p;
+        }
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+
+    return res;
+}
+
+bool miller_rabin(unsigned long long int num, int k)
+{
+    if (num <= 1 || num == 4)
+    {
+        return false;
+    }
+    if (num <= 3)
+    {
+        return true;
+    }
+
+    unsigned long long int d = num - 1;
+    while (d % 2 == 0)
+    {
+        d /= 2;
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        unsigned long long int a = 2 + rand() % (num - 4);
+        unsigned long long int x = power(a, d, num);
+
+        if (x == 1 || x == num - 1)
+        {
+            continue;
+        }
+
+        int flag = 0;
+        while (d != num - 1)
+        {
+            x = (x * x) % num;
+            d *= 2;
+
+            if (x == 1)
+            {
+                return false;
+            }
+            if (x == num - 1)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
