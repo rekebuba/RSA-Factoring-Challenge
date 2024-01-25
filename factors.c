160bf2d42aff4e3f09b2419d7774191ada557afb
rekebuba <abukiabdullahi1@gmail.com>
2024-01-25 18:50:37 +0300

    factors

---
+++ factors.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+/**
+ * main - Factorize as many numbers as possible
+ * into a product of two smaller numbers.
+ * @argc: argument count
+ * @argv: argument passed
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	FILE *file = fopen(argv[1], "r");
+	ssize_t read;
+	size_t n = 0;
+	unsigned long int num, i;
+	char *buffer = NULL, *endptr;
+
+	if (argc == 2 && *argv)
+	{
+		while ((read = getline(&buffer, &n, file)) != -1)
+		{
+			buffer[strlen(buffer) - 1] = '\0';
+			if (strlen(buffer) < 20)
+			{
+				num = strtoul(buffer, &endptr, 10);
+
+				for (i = 2; num % i != 0; i++)
+					;
+				printf("%lu=%lu*%lu\n", num, num / i, i);
+			}
+		}
+	}
+
+	return (0);
+}
