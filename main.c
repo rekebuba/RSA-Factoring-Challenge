9a61ed3980f597f384f3cdfee23da96ed69bae54
rekebuba <abukiabdullahi1@gmail.com>
2024-01-25 17:51:15 +0300

    factors

--- main.c
+++ main.c
@@ -12,22 +12,23 @@
 int main(int argc, char **argv)
 {
 	FILE *file = fopen(argv[1], "r");
-	ssize_t read, n = 0;
-	unsigned long long int num, i;
+	ssize_t read;
+	size_t n = 0;
+	unsigned long int num, i;
 	char *buffer = NULL, *endptr;
 
 	if (argc == 2 && *argv)
 	{
-		while (read = getline(&buffer, &n, file) != -1)
+		while ((read = getline(&buffer, &n, file)) != -1)
 		{
 			buffer[strlen(buffer) - 1] = '\0';
 			if (strlen(buffer) < 20)
 			{
-				num = strtoull(buffer, &endptr, 10);
+				num = strtoul(buffer, &endptr, 10);
 
 				for (i = 2; num % i != 0; i++)
 					;
-				printf("%lli=%lli*%lli\n", num, num / i, i);
+				printf("%li=%li*%li\n", num, num / i, i);
 			}
 		}
 	}
