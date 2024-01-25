f8d204e9056ec35fc37431c7c7a69bf6935cff89
rekebuba <abukiabdullahi1@gmail.com>
2024-01-25 19:58:38 +0300

    factors

--- rsa.c
+++ rsa.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-
-bool is_prime(unsigned long int num1, unsigned long int num2);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 /**
  * main - Factorize as many numbers as possible
@@ -21,7 +18,7 @@ int main(int argc, char **argv)
 	}
 	ssize_t read;
 	size_t n = 0;
-	unsigned long int num, p;
+	unsigned long int num, i;
 	char *buffer = NULL, *endptr;
 
 	while ((read = getline(&buffer, &n, file)) != -1)
@@ -31,23 +28,13 @@ int main(int argc, char **argv)
 		{
 			num = strtoul(buffer, &endptr, 10);
 
-			for (p = 2; num % p != 0 || !is_prime(p, num / p); p++)
+			for (i = 2; num % i != 0; i++)
 				;
-			printf("%lu=%lu*%lu\n", num, (num / p), p);
+			printf("%lu=%lu*%lu\n", num, num / i, i);
 		}
 	}
 
 	fclose(file);
 	free(buffer);
 	return (0);
-}
-
-bool is_prime(unsigned long int num1, unsigned long int num2)
-{
-    for (unsigned long int i = 2; i * i <= num1; i++)
-    {
-        if (num1 % i == 0 || num2 % i == 0)
-            return false;
-    }
-    return true;
 }
